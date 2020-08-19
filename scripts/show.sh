73434c580babb425c0d8a3c8114ede420fb99ff7
Sagnik Chatterjee <sagnik.chatterje1@learner.manipal.edu>
2020-08-19 15:40:29 +0530

    added description to show.sh

--- scripts/show.sh
+++ scripts/show.sh
@@ -1,1 +1,4 @@
+## Shell command to show what sre the installed packages are present and how much space are they taking up.
+
+
 dpkg-query --show --showformat='${Package;-50}\t${Installed-Size}\n' | sort -k 2 -n | grep -v deinstall | awk '{printf "%.3f MB \t %s\n", $2/(1024), $1}'
